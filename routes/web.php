eb4791d2e9f683480af341c39df583b6caa083ba
unknown <marcinxmurach995@gmail.com>
2018-12-04 15:35:53 +0100

    'test'

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {return view('app');});
-
+//Route::get('/', function () {return view('app');});
+Route::view('/{path?}', 'app');
 //Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
