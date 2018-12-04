ea8e144eaf869cca33d0f213ac2b918f77c80e4c
unknown <marcinxmurach995@gmail.com>
2018-12-04 15:39:28 +0100

    'test'

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'
+    import ReactDOM from 'react-dom'
+    import { BrowserRouter, Route, Switch } from 'react-router-dom'
+    import Header from './Header'
 
-export default class Example extends Component {
-    render() {
+    class App extends Component {
+      render () {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <BrowserRouter>
+            <div>
+              <Header />
             </div>
-        );
+          </BrowserRouter>
+        )
+      }
     }
-}
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
-}
+    ReactDOM.render(<App />, document.getElementById('app'))
