2ac28dab333ec2b40e84ee8ab8337fae43672e08
unknown <marcinxmurach995@gmail.com>
2018-12-05 14:44:16 +0100

    'y'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
-        <link href="{{secure_asset('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="app"></div>
