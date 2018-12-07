b7872e0dc33ab78ac163cbafab8acf7cc65b1845
unknown <marcinxmurach995@gmail.com>
2018-12-07 18:35:42 +0100

    'a'

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -112,7 +112,14 @@
 
         public function filtrate(Request $request)
         {
-            
+            if($request->phrase != null)
+            {
+                $phrase = '%' . $phrase . '%';
+                $articles = DB::table('articles')->join('categories', 'articles.idCategory', '=', 'categories.idCategory')->leftJoin('comments', 'comments.idArticle', '=', 'articles.idArticle')->select('articles.idArticle as idarticle', 'categories.Name as category', 'articles.idUser as user', 'articles.Title as title', 'articles.Image as image', DB::raw('SUBSTRING(articles.Content, 1, 120) as content'), 'articles.Views as views', 'articles.Visible as visible', 'articles.created_at as create_date', 'articles.updated_at as modify_date', DB::raw('(select count(*) from comments where comments.idArticle = articles.idArticle and comments.Visible = 1) as comments_count'), DB::raw('(select count(*) from user_article_likes where user_article_likes.idArticle = articles.idArticle) as likes_count'))->groupBy('articles.idArticle', 'categories.Name', 'articles.idUser', 'articles.Title', 'articles.Image', 'articles.Content', 'articles.Views', 'articles.Visible', 'articles.created_at', 'articles.updated_at', 'comments.idArticle')->where('Title', 'like', $phrase)->where('articles.Visible', '!=', 0)->orderBy('articles.idArticle', 'desc')->get();
+                return response()->json($Articles);
+            }
+            else
+                return response()->json(['status' => false]);
         }
 
         /**
