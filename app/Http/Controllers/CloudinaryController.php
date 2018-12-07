e08a5434b8bfbdfd98a2a3534c74faaa61ea50e9
unknown <marcinxmurach995@gmail.com>
2018-12-07 16:03:26 +0100

    'a'

--- app/Http/Controllers/CloudinaryController.php
+++ app/Http/Controllers/CloudinaryController.php
@@ -3,26 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use JD\Cloudder\Facades\Cloudder;
 
 class CloudinaryController extends Controller
 {
-	public function saveImage($image_url, $table, $column, $id)
+	public static function saveImage($image_url, $table, $column, $id)
 	{
-		DB::table($table)
-            ->where($column, $id)
-            ->update(['Image' => $image_url]);
+    /* DELETING OLD IMAGE (NEEDS FIXATION)
+    $currentUrl = DB::table($table)->select('Image')->where($column, $id)->value('Image');
+    $currentImageId = explode('/' . $table . '/', $currentUrl);
+    Cloudder::destroyImage($currentUrl);
+    Cloudder::delete(substr($currentImageId[1], 0, -4), ['folder' => $table]);*/
+    DB::table($table)->where($column, $id)->update(['Image' => $image_url]);
 	}
 
-    public static function uploadImage(Request $request, $table, $column, $id)
-   	{
-       $this->validate($request,['image_name'=>'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',]);
-       $image_name = time() . '.' . $request->file('image_name')->getRealPath();
-       Cloudder::upload($image_name, null);
-       list($width, $height) = getimagesize($image_name);
-       $image_url= Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height"=>$height]);
-       //Save images
-       $this->saveImage($image_url, $table, $column, $id);
-       return redirect()->back()->with('status', 'Image Uploaded Successfully');
-   	}
+  public static function uploadImage($path, $image_name, $table, $column, $id)
+  {
+    $image_name = substr($image_name, 0, -4);
+    Cloudder::upload($path, $image_name, ['folder' => $table]);
+    list($width, $height) = getimagesize($path);
+    $image_url= Cloudder::show(Cloudder::getPublicId(), ['width' => $width, 'height' => $height]);
+    $_SESSION['image'] = $image_url;
+    self::saveImage($image_url, $table, $column, $id);
+  }
 }
