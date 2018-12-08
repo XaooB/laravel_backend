412d1f766524b182ca4a637bb76d92ba68f16b8f
unknown <marcinxmurach995@gmail.com>
2018-12-08 01:10:51 +0100

    'api'

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -30,7 +30,7 @@
             $articles = DB::table('articles')->join('categories', 'articles.idCategory', '=', 'categories.idCategory')->leftJoin('comments', 'comments.idReference', '=', 'articles.idArticle')->leftJoin('user_likes', 'user_likes.idReference', '=', 'articles.idArticle')->select('articles.idArticle as idarticle', 'categories.Name as category', 'articles.idUser as user', 'articles.Title as title', 'articles.Image as image', DB::raw('SUBSTRING(articles.Content, 1, 120) as content'), 'articles.Views as views', 'articles.Visible as visible', 'articles.created_at as create_date', 'articles.updated_at as modify_date', DB::raw('(select count(*) from comments where comments.idReference = articles.idArticle and comments.Type = "article" and comments.Visible = 1) as comments_count'), DB::raw('(select count(*) from user_likes where user_likes.idReference = articles.idArticle and user_likes.Type = "article" and user_likes.Reaction = "like") as likes_count'))->groupBy('articles.idArticle', 'categories.Name', 'articles.idUser', 'articles.Title', 'articles.Image', 'articles.Content', 'articles.Views', 'articles.Visible', 'articles.created_at', 'articles.updated_at', 'comments.idReference')->where('articles.Visible', '!=', 0)->get();
             foreach ($articles as $key => $article) {
                 if(substr($article->content, -1) == '.') $article->content .= '..'; else $article->content .= '...';
-                $article->comments_count =  DB::table('comments')->where('idArticle', $article->idarticle)->count();
+                $article->comments_count =  DB::table('comments')->where('idReference', $article->idarticle)->count();
                 $userID = $article->user;
                 UsersController::buildUserData($article->user);
             }
@@ -43,7 +43,7 @@
             {
                 $articles = DB::table('articles')->join('categories', 'articles.idCategory', '=', 'categories.idCategory')->leftJoin('comments', 'comments.idReference', '=', 'articles.idArticle')->leftJoin('user_likes', 'user_likes.idReference', '=', 'articles.idArticle')->select('articles.idArticle as idarticle', 'categories.Name as category', 'articles.idUser as user', 'articles.Title as title', 'articles.Image as image', DB::raw('SUBSTRING(articles.Content, 1, 120) as content'), 'articles.Views as views', 'articles.Visible as visible', 'articles.created_at as create_date', 'articles.updated_at as modify_date', DB::raw('(select count(*) from comments where comments.idReference = articles.idArticle and comments.Type = "article" and comments.Visible = 1) as comments_count'), DB::raw('(select count(*) from user_likes where user_likes.idReference = articles.idArticle and user_likes.Type = "article" and user_likes.Reaction = "like") as likes_count'))->groupBy('articles.idArticle', 'categories.Name', 'articles.idUser', 'articles.Title', 'articles.Image', 'articles.Content', 'articles.Views', 'articles.Visible', 'articles.created_at', 'articles.updated_at', 'comments.idReference')->where('Main', '!=', 0)->where('articles.Visible', '!=', 0)->orderBy('articles.idArticle', 'desc')->limit($count)->get();
                 foreach ($articles as $key => $article) {
-                    $article->comments_count =  DB::table('comments')->where('idArticle', $article->idarticle)->count();
+                    $article->comments_count =  DB::table('comments')->where('idReference', $article->idarticle)->count();
                     if(substr($article->content, -1) == '.') $article->content .= '..'; else $article->content .= '...';
                     UsersController::buildUserData($article->user);
                 }
@@ -58,7 +58,7 @@
                 $articles = DB::table('articles')->join('categories', 'articles.idCategory', '=', 'categories.idCategory')->leftJoin('comments', 'comments.idReference', '=', 'articles.idArticle')->leftJoin('user_likes', 'user_likes.idReference', '=', 'articles.idArticle')->select('articles.idArticle as idarticle', 'categories.Name as category', 'articles.idUser as user', 'articles.Title as title', 'articles.Image as image', DB::raw('SUBSTRING(articles.Content, 1, 120) as content'), 'articles.Views as views', 'articles.Visible as visible', 'articles.created_at as create_date', 'articles.updated_at as modify_date', DB::raw('(select count(*) from comments where comments.idReference = articles.idArticle and comments.Type = "article" and comments.Visible = 1) as comments_count'), DB::raw('(select count(*) from user_likes where user_likes.idReference = articles.idArticle and user_likes.Type = "article" and user_likes.Reaction = "like") as likes_count'))->groupBy('articles.idArticle', 'categories.Name', 'articles.idUser', 'articles.Title', 'articles.Image', 'articles.Content', 'articles.Views', 'articles.Visible', 'articles.created_at', 'articles.updated_at', 'comments.idReference')->where('Main', 0)->where('articles.Visible', '!=', 0)->orderBy('articles.idArticle', 'desc')->limit($count)->get();
                 foreach ($articles as $key => $article) {
                     if(substr($article->content, -1) == '.') $article->content .= '..'; else $article->content .= '...';
-                    $article->comments_count =  DB::table('comments')->where('idArticle', $article->idarticle)->count();
+                    $article->comments_count =  DB::table('comments')->where('idReference', $article->idarticle)->count();
                     UsersController::buildUserData($article->user);
                 }
                 return response()->json($articles);
@@ -72,7 +72,7 @@
                 if($article->visible)
                 {
                     if(substr($article->content, -1) == '.') $article->content .= '..'; else $article->content .= '...';
-                    $article->comments_count =  DB::table('comments')->where('idArticle', $article->idarticle)->count();
+                    $article->comments_count =  DB::table('comments')->where('idReference', $article->idarticle)->count();
                     UsersController::buildUserData($article->user);
                 }
             }
