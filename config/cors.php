6a25a9ca06f304db029d1f47ee2a9310110fdbcb
unknown <marcinxmurach995@gmail.com>
2018-12-19 20:00:03 +0100

    'react'

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['portal-wertykalny.herokuapp.com'],
+    'allowedOrigins' => ['*'], //portal-wertykalny.herokuapp.com
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
