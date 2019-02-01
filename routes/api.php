24dfdb0baa334a0543da9c499c86dd45c8ccf8e1
unknown <marcinxmurach995@gmail.com>
2019-02-01 23:13:22 +0100

    'api'

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::get('test', function() {
 });
 
 Route::get('articles_panel', 'ArticlesController@panel')->name('articles.panel');
-Route::get('users_panel', 'UsersController@panel')->name('users.panel');
+Route::get('users_panel/{days}', 'UsersController@panel')->name('users.panel');
 Route::get('comments_panel', 'CommentsController@panel')->name('comments.panel');
 
 // Use middleware to allow Client-side use API
