7b0c1fc8502d6410aa1e6b5814196ecf78ddb87a
unknown <marcinxmurach995@gmail.com>
2019-02-12 04:17:36 +0100

    'ga'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Polska - fanowski portal hiszpańskiego klubu Real Madrid</title>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-134339010-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-134339010-1');
+		</script>
     </head>
     <body>
         <div id="app"></div>
