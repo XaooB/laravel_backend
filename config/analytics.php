851f16943b43d79a56da090935ea75e9cd10e3f1
unknown <marcinxmurach995@gmail.com>
2019-02-12 15:19:19 +0100

    'google-analytics'

--- config/analytics.php
+++ config/analytics.php
@@ -12,7 +12,7 @@ return [
      * to learn how to get this file. You can also pass the credentials as an array
      * instead of a file path.
      */
-    'service_account_credentials_json' => env('ANALYTICS_CREDENTIALS'),
+    'service_account_credentials_json' => storage_path('app/public/package.json'),
 
     /*
      * The amount of minutes the Google API responses will be cached.
