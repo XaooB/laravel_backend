ed819c9d2b90f404fd9c99c8c8f5cbf1db98f8d9
github_Marcin <40297337+Marcin250@users.noreply.github.com>
2019-04-30 02:41:06 +0200

    Add files via upload

---
+++ resources/views/emails.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>{{$header}}</h1>
+        <p>{{$content}}</p>
+    </body>
+</html>
