2f7ec0c74edd74f56603d08386c2f93cdbc00636
unknown <marcinxmurach995@gmail.com>
2019-05-12 21:50:01 +0200

    'test'

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'wic@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Wic'),
+        'address' => env('MAIL_FROM_ADDRESS', 'oauth2testbackend@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Admin'),
     ],
 
     /*
